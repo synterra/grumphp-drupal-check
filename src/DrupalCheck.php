6e8fccefb89b20ed50fc387f2c5a7998de5bfeea
Sady <sadysierraltao@gmail.com>
2019-08-15 19:03:13 +0200

    Add drupal check command

---
+++ src/DrupalCheck.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace GrumphpDrupalCheck;
+
+use GrumPHP\Runner\TaskResult;
+use GrumPHP\Runner\TaskResultInterface;
+use GrumPHP\Task\Context\ContextInterface;
+use GrumPHP\Task\Context\GitPreCommitContext;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use GrumPHP\Task\AbstractExternalTask;
+
+/**
+ * Drupal check task.
+ */
+class DrupalCheck extends AbstractExternalTask
+{
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getName(): string
+  {
+      return 'drupalcheck';
+  }
+
+  /**
+   * @param ContextInterface $context
+   *
+   * @return bool
+   */
+  public function canRunInContext(ContextInterface $context)
+  {
+      return ($context instanceof GitPreCommitContext);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getConfigurableOptions(): OptionsResolver
+  {
+      $resolver = new OptionsResolver();
+      return $resolver;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function run(ContextInterface $context): TaskResultInterface
+  {
+    /** @var \GrumPHP\Collection\FilesCollection $files */
+    $files = $context->getFiles();
+    $triggered_by = [
+      'php',
+      'inc',
+      'module',
+      'install',
+      'profile',
+      'theme',
+    ];
+    $files = $files->extensions($triggered_by);
+    if (0 === \count($files)) {
+        return TaskResult::createSkipped($this, $context);
+    }
+    $arguments = $this->processBuilder->createArgumentsForCommand('drupal-check');
+    $arguments->add('--deprecations');
+    $arguments->add('--no-progress');
+    $arguments->addFiles($files);
+    $process = $this->processBuilder->buildProcess($arguments);
+    $process->run();
+    if (!$process->isSuccessful()) {
+        $output = $this->formatter->format($process);
+        return TaskResult::createFailed($this, $context, $output);
+    }
+    return TaskResult::createPassed($this, $context);
+  }
+
+}
